97a11d8e9cd477d9fea3ebaf33161ce0ecd59fa8
Kamil Ignacak <acerion@users.sourceforge.net>
2011-10-05 22:52:01 +0200

    New file with code that appeared (with small variations) in three different files (cw.c, cwcp.c, xcwcp/main.cc). Creating cw_common.[ch] should make some things more consistent and easier to maintain. Client code using functions from cw_common.c should be shorter. I'm introducing cw_config_t data type and 'config' variable storing some parameters (mostly those that can be passed in command line). The type is a bridge between cw_common.c and client code.

---
+++ src/cwutils/cw_common.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2001-2006  Simon Baldwin (simon_baldwin@yahoo.com)
+ * Copyright (C) 2011       Kamil Ignacak (acerion@wp.pl.)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+/* Code that is common for all _applications_ from unixcw package.
+   Wrappers for some cwlib functions, that probably don't belong
+   to cwlib.c. */
+
+
+#include <stdio.h>  /* fprintf(stderr, ...) */
+#include <stdlib.h> /* malloc() / free() */
+
+#include "cwlib.h"
+#include "cw_common.h"
+
+static void cw_generator_apply_config(cw_config_t *config);
+
+
+enum { FALSE = 0, TRUE = !FALSE };
+
+
+cw_config_t *cw_config_new(void)
+{
+	cw_config_t *config = (cw_config_t *) malloc(sizeof (cw_config_t));
+	if (!config) {
+		fprintf(stderr, "cwlib: can't allocate memory for configuration\n");
+		return NULL;
+	}
+
+	config->audio_system = CW_AUDIO_NONE;
+	config->audio_device = NULL;
+	config->send_speed = CW_INITIAL_SEND_SPEED;
+	config->frequency = CW_INITIAL_FREQUENCY;
+	config->volume = CW_INITIAL_VOLUME;
+	config->gap = CW_INITIAL_GAP;
+	config->weighting = CW_INITIAL_WEIGHTING;
+	config->practice_time = CW_PRACTICE_TIME_INITIAL;
+	config->input_file = NULL;
+	config->output_file = NULL;
+
+	config->is_cw = 0;
+	config->has_practice_time = 0;
+	config->has_outfile = 0;
+
+	config->do_echo = TRUE;
+	config->do_errors = TRUE;
+	config->do_commands = TRUE;
+	config->do_combinations = TRUE;
+	config->do_comments = TRUE;
+
+	return config;
+}
+
+
+
+void cw_config_delete(cw_config_t **config)
+{
+	if (*config) {
+		if ((*config)->audio_device) {
+			free((*config)->audio_device);
+			(*config)->audio_device = NULL;
+		}
+		if ((*config)->input_file) {
+			free((*config)->input_file);
+			(*config)->input_file = NULL;
+		}
+		if ((*config)->output_file) {
+			free((*config)->output_file);
+			(*config)->output_file = NULL;
+		}
+		free(*config);
+		*config = NULL;
+	}
+
+	return;
+}
+
+
+
+
+/*
+ * cw_config_is_valid()
+ *
+ * Check consistency and correctness of configuration.
+ */
+int cw_config_is_valid(cw_config_t *config)
+{
+	/* Deal with odd argument combinations. */
+        if (config->audio_device) {
+		if (config->audio_system == CW_AUDIO_SOUNDCARD) {
+			fprintf(stderr, "cwlib: a device has been specified for 'soundcard' argument\n");
+			fprintf(stderr, "cwlib: a device can be specified only for 'console', 'oss' or 'alsa'\n");
+			return 0;
+		} else {
+			; /* audio_system is one that accepts custom "audio device" */
+		}
+	} else {
+		; /* no custom "audio device" specified, a default will be used */
+	}
+
+	return 1;
+}
+
+
+
+
+
+
+/* A wrapper for common functionality.
+   For a lack of better place I put it in this file */
+int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
+{
+
+	if (config->audio_system == CW_AUDIO_NONE
+	    || config->audio_system == CW_AUDIO_OSS
+	    || config->audio_system == CW_AUDIO_SOUNDCARD) {
+
+		int rv = -1;
+		if (cw_is_oss_possible(config->audio_device)) {
+			rv = cw_generator_new(CW_AUDIO_OSS, config->audio_device);
+			if (rv != 1) {
+				fprintf(stderr,
+					"%s: failed to open OSS output with device \"%s\"\n",
+					argv0, cw_get_soundcard_device());
+			} else {
+				cw_generator_apply_config(config);
+				return 1;
+			}
+		}
+		/* fall through to try with next audio system type */
+	}
+
+
+	if (config->audio_system == CW_AUDIO_NONE
+	    || config->audio_system == CW_AUDIO_ALSA
+	    || config->audio_system == CW_AUDIO_SOUNDCARD) {
+
+		int rv = -1;
+		if (cw_is_alsa_possible(config->audio_device)) {
+			rv = cw_generator_new(CW_AUDIO_ALSA, config->audio_device);
+			if (rv != 1) {
+				fprintf(stderr,
+					"%s: failed to open ALSA output with device \"%s\"\n",
+					argv0, cw_get_soundcard_device());
+			} else {
+				cw_generator_apply_config(config);
+				return 1;
+			}
+		}
+		/* fall through to try with next audio system type */
+	}
+
+
+	if (config->audio_system == CW_AUDIO_NONE
+	    || config->audio_system == CW_AUDIO_CONSOLE) {
+
+		int rv = -1;
+		if (cw_is_console_possible(config->audio_device)) {
+			rv = cw_generator_new(CW_AUDIO_CONSOLE, config->audio_device);
+			if (rv != 1) {
+				fprintf(stderr,
+					"%s: failed to open console output with device \"%s\"\n",
+					argv0, cw_get_soundcard_device());
+			} else {
+				cw_generator_apply_config(config);
+				return 1;
+			}
+		}
+		/* fall through to try with next audio system type */
+	}
+
+	/* there is no next audio system type to try */
+	return -1;
+}
+
+
+
+
+
+void cw_generator_apply_config(cw_config_t *config)
+{
+	cw_set_frequency(config->frequency);
+	cw_set_volume(config->volume);
+	cw_set_send_speed(config->send_speed);
+	cw_set_gap(config->gap);
+	cw_set_weighting(config->weighting);
+
+	return;
+}
+
---
+++ src/cwutils/cw_common.h
@@ -0,0 +1,59 @@
+#ifndef _CW_COMMON_H
+#define _CW_COMMON_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define CW_PRACTICE_TIME_MIN        1
+#define CW_PRACTICE_TIME_MAX       99
+#define CW_PRACTICE_TIME_INITIAL   15
+#define CW_PRACTICE_TIME_STEP       1
+
+
+typedef struct {
+	int audio_system;
+	char *audio_device;
+	int send_speed;
+	int frequency;
+	int volume;
+	int gap;
+	int weighting;
+	int practice_time;
+	char *input_file;
+	char *output_file;
+
+	int is_cw;
+	int has_practice_time;
+	int has_outfile;
+
+	/*
+	 * Program-specific state variables, settable from the command line, or from
+	 * embedded input stream commands.  These options may be set by the embedded
+	 * command parser to values other than strictly TRUE or FALSE; all non-zero
+	 * values are equivalent to TRUE.
+	 *
+	 * These fields are used only in cw.
+	 */
+	int do_echo;           /* Echo characters */
+        int do_errors;         /* Print error messages to stderr */
+        int do_commands;       /* Execute embedded commands */
+        int do_combinations;   /* Execute [...] combinations */
+        int do_comments;       /* Allow {...} as comments */
+} cw_config_t;
+
+
+
+extern void cw_print_help(const char *program_name, cw_config_t *config);
+extern cw_config_t *cw_config_new(void);
+extern void cw_config_delete(cw_config_t **config);
+extern int cw_config_is_valid(cw_config_t *config);
+extern int cw_generator_new_from_config(cw_config_t *config, const char *argv0);
+
+#if defined(__cplusplus)
+}
+#endif
+
+
+#endif /* _CW_COMMON_H */
+
