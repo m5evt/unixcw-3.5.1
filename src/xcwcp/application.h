2c5c98338a888020ab8ff2a8666e3b994357aba5
Kamil Ignacak <acerion@users.sourceforge.net>
2012-04-27 21:03:07 +0200

    Updating type of an argument function.

--- src/xcwcp/application.h
+++ src/xcwcp/application.h
@@ -146,7 +146,7 @@ class Application : public QMainWindow
   // the whole class, and an instance version for each object.  The class
   // version calls the relevant instance version, based on which instance is
   // the current registered libcw user.
-  static void libcw_keying_event_static (void *, int key_state);
+  static void libcw_keying_event_static (void *, bool key_state);
   void libcw_keying_event (int key_state);
 
   // Wrappers for creating UI.
