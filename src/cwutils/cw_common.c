14e802ca0a9cafec6e2351b770aef0072c27d756
Kamil Ignacak <acerion@users.sourceforge.net>
2012-05-06 22:30:57 +0200

    Refactoring.
    Replacing TRUE/FALSE with true/false.
    Adding 'program_name' field to config data type and making use of it (argument list of some functions has changed).
    Adding/updating comments. Adding some checks of return values.

--- src/cwutils/cw_common.c
+++ src/cwutils/cw_common.c
@@ -18,28 +18,64 @@
  */
 
 
-/* Code that is common for all _applications_ from unixcw package.
-   Wrappers for some libcw functions, that probably don't belong
-   to libcw.c. */
+/**
+   \file cw_common.c
 
+   Code that is common for all _applications_ from unixcw package.
+   Wrappers for some libcw functions, that probably don't belong to libcw.c.
+*/
+
+#define _BSD_SOURCE /* strdup() */
+
+#include "config.h"
 
 #include <stdio.h>  /* fprintf(stderr, ...) */
 #include <stdlib.h> /* malloc() / free() */
 
+#if defined(HAVE_STRING_H)
+# include <string.h>
+#endif
+
+#if defined(HAVE_STRINGS_H)
+# include <strings.h>
+#endif
+
+
 #include "libcw.h"
 #include "cw_common.h"
 
-static void cw_generator_apply_config(cw_config_t *config);
+static int cw_generator_apply_config(cw_config_t *config);
 
 
-enum { FALSE = 0, TRUE = !FALSE };
 
 
-cw_config_t *cw_config_new(void)
+
+/**
+   Create new configuration with default values
+
+   Function returns pointer to config variable, with fields
+   of config initialized to valid default values.
+
+   \param program_name - human-readable name of application calling the function
+
+   \return pointer to config on success
+   \return NULL on failure
+*/
+cw_config_t *cw_config_new(const char *program_name)
 {
 	cw_config_t *config = (cw_config_t *) malloc(sizeof (cw_config_t));
 	if (!config) {
-		fprintf(stderr, "libcw: can't allocate memory for configuration\n");
+		fprintf(stderr, "%s: can't allocate memory for configuration\n", program_name);
+		return NULL;
+	}
+
+	config->program_name = strdup(program_name);
+	if (!(config->program_name)) {
+		fprintf(stderr, "%s: can't allocate memory for program name\n", program_name);
+
+		free(config);
+		config = NULL;
+
 		return NULL;
 	}
 
@@ -54,25 +90,38 @@ cw_config_t *cw_config_new(void)
 	config->input_file = NULL;
 	config->output_file = NULL;
 
-	config->is_cw = 0;
-	config->has_practice_time = 0;
-	config->has_outfile = 0;
-	config->has_infile = true; /* TODO: set to false by default, update sources */
+	config->is_cw = false;
+	config->has_practice_time = false;
+	config->has_outfile = false;
+	config->has_infile = true;
 
-	config->do_echo = TRUE;
-	config->do_errors = TRUE;
-	config->do_commands = TRUE;
-	config->do_combinations = TRUE;
-	config->do_comments = TRUE;
+	config->do_echo = true;
+	config->do_errors = true;
+	config->do_commands = true;
+	config->do_combinations = true;
+	config->do_comments = true;
 
 	return config;
 }
 
 
 
+
+
+/**
+   \brief Delete configuration variable
+
+   Deallocate given configuration, assign NULL to \p config
+
+   \param config - configuration variable to deallocate
+*/
 void cw_config_delete(cw_config_t **config)
 {
 	if (*config) {
+		if ((*config)->program_name) {
+			free((*config)->program_name);
+			(*config)->program_name = NULL;
+		}
 		if ((*config)->audio_device) {
 			free((*config)->audio_device);
 			(*config)->audio_device = NULL;
@@ -95,11 +144,23 @@ void cw_config_delete(cw_config_t **config)
 
 
 
-/*
- * cw_config_is_valid()
- *
- * Check consistency and correctness of configuration.
- */
+
+/**
+   \brief Validate configuration
+
+   Check consistency and correctness of configuration.
+
+   Currently the function only checks if "audio device" command line
+   argument has been specified at the same time when "soundcard"
+   has been specified as audio system. This is an inconsistency as
+   you can specify audio device only for specific audio system ("soundcard"
+   is just a general audio system).
+
+   \param config - configuration to validate
+
+   \return true if configuration is valid
+   \return false if configuration is invalid
+*/
 int cw_config_is_valid(cw_config_t *config)
 {
 	/* Deal with odd argument combinations. */
@@ -107,7 +168,7 @@ int cw_config_is_valid(cw_config_t *config)
 		if (config->audio_system == CW_AUDIO_SOUNDCARD) {
 			fprintf(stderr, "libcw: a device has been specified for 'soundcard' argument\n");
 			fprintf(stderr, "libcw: a device can be specified only for 'console', 'oss' or 'alsa'\n");
-			return CW_FAILURE;
+			return false;
 		} else {
 			; /* audio_system is one that accepts custom "audio device" */
 		}
@@ -115,7 +176,7 @@ int cw_config_is_valid(cw_config_t *config)
 		; /* no custom "audio device" specified, a default will be used */
 	}
 
-	return CW_SUCCESS;
+	return true;
 }
 
 
@@ -123,9 +184,20 @@ int cw_config_is_valid(cw_config_t *config)
 
 
 
-/* A wrapper for common functionality.
-   For a lack of better place I put it in this file */
-int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
+/**
+   \brief Create new cw generator, apply given configuration
+
+   Create new cw generator (using audio system from given \p config), and
+   then apply rest of parameters from \p config to that generator.
+
+   \p config should be first created with cw_config_new().
+
+   \param config - configuration to be applied to generator
+
+   \return CW_SUCCESS on success
+   \return CW_FAILURE on failure
+*/
+int cw_generator_new_from_config(cw_config_t *config)
 {
 	if (config->audio_system == CW_AUDIO_NONE
 	    || config->audio_system == CW_AUDIO_PA
@@ -133,15 +205,17 @@ int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
 
 		if (cw_is_pa_possible(config->audio_device)) {
 			if (cw_generator_new(CW_AUDIO_PA, config->audio_device)) {
-				cw_generator_apply_config(config);
-				return CW_SUCCESS;
+				if (cw_generator_apply_config(config)) {
+					return CW_SUCCESS;
+				} else {
+					fprintf(stderr, "%s: failed to apply configuration\n", config->program_name);
+					return CW_FAILURE;
+				}
 			} else {
-				fprintf(stderr,
-					"%s: failed to open PulseAudio output with device \"%s\"\n",
-					argv0, cw_get_soundcard_device());
+				fprintf(stderr, "%s: failed to open PulseAudio output\n", config->program_name);
 			}
 		} else {
-			fprintf(stderr, "%s: PulseAudio output not available\n", argv0);
+			fprintf(stderr, "%s: PulseAudio output not available (device: %s)\n", config->program_name, config->audio_device);
 		}
 		/* fall through to try with next audio system type */
 	}
@@ -152,15 +226,19 @@ int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
 
 		if (cw_is_oss_possible(config->audio_device)) {
 			if (cw_generator_new(CW_AUDIO_OSS, config->audio_device)) {
-				cw_generator_apply_config(config);
-				return CW_SUCCESS;
+				if (cw_generator_apply_config(config)) {
+					return CW_SUCCESS;
+				} else {
+					fprintf(stderr, "%s: failed to apply configuration\n", config->program_name);
+					return CW_FAILURE;
+				}
 			} else {
 				fprintf(stderr,
 					"%s: failed to open OSS output with device \"%s\"\n",
-					argv0, cw_get_soundcard_device());
+					config->program_name, cw_get_soundcard_device());
 			}
 		} else {
-			fprintf(stderr, "%s: OSS output not available\n", argv0);
+			fprintf(stderr, "%s: OSS output not available (device: %s)\n", config->program_name, config->audio_device);
 		}
 		/* fall through to try with next audio system type */
 	}
@@ -172,15 +250,19 @@ int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
 
 		if (cw_is_alsa_possible(config->audio_device)) {
 			if (cw_generator_new(CW_AUDIO_ALSA, config->audio_device)) {
-				cw_generator_apply_config(config);
-				return CW_SUCCESS;
+				if (cw_generator_apply_config(config)) {
+					return CW_SUCCESS;
+				} else {
+					fprintf(stderr, "%s: failed to apply configuration\n", config->program_name);
+					return CW_FAILURE;
+				}
 			} else {
 				fprintf(stderr,
 					"%s: failed to open ALSA output with device \"%s\"\n",
-					argv0, cw_get_soundcard_device());
+					config->program_name, cw_get_soundcard_device());
 			}
 		} else {
-			fprintf(stderr, "%s: ALSA output not available\n", argv0);
+			fprintf(stderr, "%s: ALSA output not available (device: %s)\n", config->program_name, config->audio_device);
 		}
 		/* fall through to try with next audio system type */
 	}
@@ -191,16 +273,19 @@ int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
 
 		if (cw_is_console_possible(config->audio_device)) {
 			if (cw_generator_new(CW_AUDIO_CONSOLE, config->audio_device)) {
-				cw_generator_apply_config(config);
-				return CW_SUCCESS;
+				if (cw_generator_apply_config(config)) {
+					return CW_SUCCESS;
+				} else {
+					fprintf(stderr, "%s: failed to apply configuration\n", config->program_name);
+					return CW_FAILURE;
+				}
 			} else {
 				fprintf(stderr,
 					"%s: failed to open console output with device %s\n",
-					/* FIXME: this condition doesn't look 100% correct */
-					argv0, cw_get_soundcard_device() ? cw_get_soundcard_device() : config->audio_device);
+					config->program_name, cw_get_console_device() ? cw_get_console_device() : config->audio_device);
 			}
 		} else {
-			fprintf(stderr, "%s: console output not available\n", argv0);
+			fprintf(stderr, "%s: console output not available (device: %s)\n", config->program_name, config->audio_device);
 		}
 		/* fall through to try with next audio system type */
 	}
@@ -213,21 +298,49 @@ int cw_generator_new_from_config(cw_config_t *config, const char *argv0)
 
 
 
-void cw_generator_apply_config(cw_config_t *config)
-{
-	cw_set_frequency(config->frequency);
-	cw_set_volume(config->volume);
-	cw_set_send_speed(config->send_speed);
-	cw_set_gap(config->gap);
-	cw_set_weighting(config->weighting);
+/**
+   \brief Apply given configuration to a generator
 
-	return;
+   Function applies frequency, volume, sending speed, gap and weighting
+   to current generator. The generator should exist (it should be created
+   by cw_generator_new().
+
+   The function is just a wrapper for few common function calls, to be used
+   in cw_generator_new_from_config().
+
+   \param config - current configuration
+
+   \return CW_SUCCESS on success
+   \return CW_FAILURE on failure
+*/
+int cw_generator_apply_config(cw_config_t *config)
+{
+	if (!cw_set_frequency(config->frequency)) {
+		return CW_FAILURE;
+	}
+	if (!cw_set_volume(config->volume)) {
+		return CW_FAILURE;
+	}
+	if (!cw_set_send_speed(config->send_speed)) {
+		return CW_FAILURE;
+	}
+	if (!cw_set_gap(config->gap)) {
+		return CW_FAILURE;
+	}
+	if (!cw_set_weighting(config->weighting)) {
+		return CW_FAILURE;
+	}
+
+	return CW_SUCCESS;
 }
 
 
 
 
 
+/**
+   \brief Generate a tone that indicates a start
+*/
 void cw_start_beep(void)
 {
 	cw_flush_tone_queue();
@@ -241,6 +354,9 @@ void cw_start_beep(void)
 
 
 
+/**
+   \brief Generate a tone that indicates an end
+*/
 void cw_end_beep(void)
 {
       cw_flush_tone_queue();
