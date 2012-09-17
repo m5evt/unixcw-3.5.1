f0c970320a62316551db67def942b1c767679188
Kamil Ignacak <acerion@users.sourceforge.net>
2012-09-17 20:13:54 +0200

    New file with old content related to ALSA audio backend, moved from libcw.[ch]. The module now has slightly different API than previous set of ALSA functions, thanks to new set of function pointers in cw_gen_t, and thanks to cw_alsa_configure().

---
+++ src/libcw/libcw_alsa.h
@@ -0,0 +1,25 @@
+/*
+  This file is a part of unixcw project.
+  unixcw project is covered by GNU General Public License.
+*/
+
+
+#ifndef H_CW_ALSA
+#define H_CW_ALSA
+
+#if defined(LIBCW_WITH_ALSA)
+
+
+#include "libcw_internal.h"
+
+
+
+bool cw_is_alsa_possible(const char *device);
+int  cw_alsa_configure(cw_gen_t *gen, const char *device);
+void cw_alsa_drop(cw_gen_t *gen);
+
+
+
+#endif // #if defined(LIBCW_WITH_ALSA)
+
+#endif // #ifndef H_CW_ALSA
