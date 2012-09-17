f39bb63bc58424f602a2b8baad20f14f33a4a556
Kamil Ignacak <acerion@users.sourceforge.net>
2012-09-17 20:15:51 +0200

    New file with old content related to console buzzer backend, moved from libcw.[ch].

---
+++ src/libcw/libcw_console.h
@@ -0,0 +1,23 @@
+/*
+  This file is a part of unixcw project.
+  unixcw project is covered by GNU General Public License.
+*/
+
+
+#ifndef H_CW_CONSOLE
+#define H_CW_CONSOLE
+
+
+
+#include "libcw.h"
+#include "libcw_internal.h"
+
+
+bool cw_is_console_possible(const char *device);
+int  cw_console_configure(cw_gen_t *gen, const char *device);
+int  cw_console_write(cw_gen_t *gen, cw_tone_t *tone);
+void cw_console_silence(cw_gen_t *gen);
+
+
+
+#endif //#ifndef H_CW_CONSOLE
