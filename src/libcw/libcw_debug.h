c19df2a8f4b657aef0989c3b5fdf22e7f4310c31
Kamil Ignacak <acerion@users.sourceforge.net>
2012-09-17 20:12:49 +0200

    Few items moved from libcw.c.

--- src/libcw/libcw_debug.h
+++ src/libcw/libcw_debug.h
@@ -20,7 +20,7 @@
 #ifndef _LIBCW_DEBUG_H
 #define _LIBCW_DEBUG_H
 
-
+#include <stdbool.h>
 
 
 #if defined(__cplusplus)
@@ -53,20 +53,48 @@ cw_debug_t *cw_debug2_new(const char *filename);
 void        cw_debug2_delete(cw_debug_t **debug);
 void        cw_debug2(cw_debug_t *debug, int flag, int event);
 void        cw_debug2_flush(cw_debug_t *debug);
+bool        cw_is_debugging_internal(unsigned int flag);
 
+
+/* macro supporting multiple arguments */
+#define cw_debug(flag, ...)				\
+	{						\
+		if (cw_is_debugging_internal(flag)) {	\
+			fprintf(stderr, "libcw: ");	\
+			fprintf(stderr, __VA_ARGS__);	\
+			fprintf(stderr, "\n");		\
+		}					\
+	}
+
+
+	/* Debugging message for library developer */
+#ifdef LIBCW_WITH_DEV
+#define cw_dev_debug(...)						\
+	{								\
+		fprintf(stderr, "libcw: ");				\
+		fprintf(stderr, "%s: %d: ", __func__, __LINE__);	\
+		fprintf(stderr, __VA_ARGS__);				\
+		fprintf(stderr, "\n");					\
+	}
+#else
+#define cw_dev_debug(...) {}
+#endif
 
 
 enum {
 	CW_DEBUG_EVENT_TONE_LOW  = 0,         /* Tone with non-zero frequency. */
 	CW_DEBUG_EVENT_TONE_MID,              /* A state between LOW and HIGH, probably unused. */
 	CW_DEBUG_EVENT_TONE_HIGH,             /* Tone with zero frequency. */
-
 	CW_DEBUG_EVENT_TQ_JUST_EMPTIED,       /* A last tone from libcw's queue of tones has been dequeued, making the queue empty. */
 	CW_DEBUG_EVENT_TQ_NONEMPTY,           /* A tone from libcw's queue of tones has been dequeued, but the queue is still non-empty. */
 	CW_DEBUG_EVENT_TQ_STILL_EMPTY         /* libcw's queue of tones has been asked for tone, but there were no tones on the queue. */
 };
 
 
+#if CW_DEV_RAW_SINK
+int  cw_dev_debug_raw_sink_write_internal(cw_gen_t *gen);
+void cw_dev_debug_print_generator_setup(cw_gen_t *gen);
+#endif
 
 
 #if defined(__cplusplus)
