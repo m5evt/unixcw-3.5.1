07d4538001be8add9173ffe10f165b8d68d6afec
acerion <acerion@wp.pl>
2013-10-17 21:05:37 +0200

    Reformatting code.

--- src/xcwcp/receiver.h
+++ src/xcwcp/receiver.h
@@ -37,51 +37,56 @@ namespace cw {
 class Display;
 class Mode;
 
-class Receiver
-{
+class Receiver {
  public:
-  Receiver (Display *display)
-    : display_ (display),
-      is_pending_space_ (false), libcw_receive_errno_ (0),
-      tracked_key_state_ (false) { }
+	Receiver (Display *display)
+		: display_ (display),
+		is_pending_inter_word_space_ (false),
+		libcw_receive_errno_ (0),
+		tracked_key_state_ (false) { }
 
-  // Poll timeout handler, and keypress and mouse event handlers.
-  void poll (const Mode *current_mode);
-  void handle_key_event (QKeyEvent *event, const Mode *current_mode,
-                         bool is_reverse_paddles);
-  void handle_mouse_event (QMouseEvent *event, const Mode *current_mode,
-                           bool is_reverse_paddles);
+	// Poll timeout handler.
+	void poll(const Mode *current_mode);
 
-  // CW library keying event handler.
-  void handle_libcw_keying_event (int key_state);
+	// Keypress event handler.
+	void handle_key_event(QKeyEvent *event, const Mode *current_mode,
+			      bool is_reverse_paddles);
 
-  // Clear out queued data on stop, mode change, etc.
-  void clear ();
+	// Mouse button press event handler.
+	void handle_mouse_event(QMouseEvent *event, const Mode *current_mode,
+				bool is_reverse_paddles);
+
+	// CW library keying event handler.
+	void handle_libcw_keying_event(int key_state);
+
+	// Clear out queued data on stop, mode change, etc.
+	void clear();
 
  private:
-  // Display used for output.
-  Display *display_;
+	// Display used for output.
+	Display *display_;
 
-  // Flag indicating if receive polling has received a character, and may need
-  // to augment it with a word space on a later poll, and possible receive
-  // errno detected in signal handler context and needing to be passed to
-  // the foreground.
-  volatile bool is_pending_space_;
-  volatile int libcw_receive_errno_;
+	// Flag indicating if receive polling has received a character, and may need
+	// to augment it with a word space on a later poll.
+	volatile bool is_pending_inter_word_space_;
 
-  // Safety flag to ensure that we keep the library in sync with keyer events.
-  // Without, there's a chance that of a on-off event, one half will go to one
-  // application instance, and the other to another instance.
-  volatile int tracked_key_state_;
+	// Flag indicating possible receive errno detected in signal
+	// handler context and needing to be passed to the foreground.
+	volatile int libcw_receive_errno_;
 
-  // Poll primitives to handle receive errors, characters, and spaces.
-  void poll_report_receive_error ();
-  void poll_receive_character ();
-  void poll_receive_space ();
+	// Safety flag to ensure that we keep the library in sync with keyer events.
+	// Without, there's a chance that of a on-off event, one half will go to one
+	// application instance, and the other to another instance.
+	volatile int tracked_key_state_;
 
-  // Prevent unwanted operations.
-  Receiver (const Receiver &);
-  Receiver &operator= (const Receiver &);
+	// Poll primitives to handle receive errors, characters, and spaces.
+	void poll_report_receive_error();
+	void poll_receive_character();
+	void poll_receive_space();
+
+	// Prevent unwanted operations.
+	Receiver (const Receiver &);
+	Receiver &operator= (const Receiver &);
 };
 
 }  // cw namespace
