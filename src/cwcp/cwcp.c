2ec91d1522a39f2c4263405e02725a54bd1ac52a
acerion <acerion@wp.pl>
2013-12-09 23:17:21 +0100

    Fixing version number.

--- src/cwcp/cwcp.c
+++ src/cwcp/cwcp.c
@@ -860,7 +860,7 @@ bool mode_is_sending_active(void)
  * character limit imposed by ISO C89 on string literal lengths.
  */
 static const char *const INTRODUCTION = N_(
-  "UNIX/Linux Morse Tutor v3.2.0\n"
+  "UNIX/Linux Morse Tutor v3.3.0\n"
   "Copyright (C) 1997-2006 Simon Baldwin\n"
   "Copyright (C) 2011-2013 Kamil Ignacak\n"
   "---------------------------------------------------------\n"
