9f582e87515e594981c555c8f663db4a5300b3da
acerion <acerion@wp.pl>
2014-07-21 22:46:38 +0200

    New file with code from libcw.c that is related to characters representation, prosigns and phonetics.

---
+++ src/libcw/libcw_data.h
@@ -0,0 +1,46 @@
+#ifndef H_LIBCW_DATA
+#define H_LIBCW_DATA
+
+
+#include <stdbool.h>
+
+#include "libcw_internal.h"
+
+
+
+
+
+typedef struct cw_entry_struct{
+	const char character;              /* Character represented */
+	const char *const representation;  /* Dot-dash shape of the character */
+} cw_entry_t;
+
+
+
+
+
+/* functions handling representation of a character;
+   representation looks like this: ".-" for "a", "--.." for "z", etc. */
+int          cw_representation_lookup_init_internal(const cw_entry_t *lookup[]);
+int          cw_representation_to_character_internal(const char *representation);
+__attribute__((unused)) int cw_representation_to_character_direct_internal(const char *representation);
+unsigned int cw_representation_to_hash_internal(const char *representation);
+const char  *cw_character_to_representation_internal(int c);
+
+
+const char *cw_lookup_procedural_character_internal(int c, bool *is_usually_expanded);
+
+
+
+
+#ifdef LIBCW_UNIT_TESTS
+
+unsigned int test_cw_representation_to_hash_internal(void);
+unsigned int test_cw_representation_to_character_internal(void);
+unsigned int test_cw_representation_to_character_internal_speed(void);
+
+#endif /* #ifdef LIBCW_UNIT_TESTS */
+
+
+
+#endif /* #ifndef H_LIBCW_DATA */
