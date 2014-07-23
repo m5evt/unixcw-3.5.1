ac4526da4261c972796bea0edcb7d315ab2bc711
acerion <acerion@wp.pl>
2014-07-23 22:41:17 +0200

    New file with headers of functions from libcw_test.c and with some test utilities.

---
+++ src/libcw/libcw_test.h
@@ -0,0 +1,25 @@
+#ifndef H_LIBCW_TEST
+#define H_LIBCW_TEST
+
+
+#include <stddef.h> /* size_t */
+
+
+static const int cw_test_print_width = 75;
+
+
+#define CW_TEST_PRINT_TEST_RESULT(m_failure, m_n) {			\
+		printf("%*s\n", (cw_test_print_width - m_n), m_failure ? "failure" : "success"); \
+	}
+
+#define CW_TEST_PRINT_FUNCTION_COMPLETED(m_func_name) {			\
+		int m = printf("libcw: %s(): ", m_func_name);		\
+		printf("%*s\n\n", cw_test_print_width - m, "completed");	\
+	}
+
+
+int cw_test_args(int argc, char *const argv[], char *sound_systems, size_t systems_max);
+void cw_test_print_help(const char *progname);
+
+
+#endif /* #ifndef H_LIBCW_TEST */
