7773b5f7a86253debee32ff84b907992f4c1b313
acerion <acerion@wp.pl>
2014-07-27 18:01:06 +0200

    Putting project information notice on top of the file. White spaces.

--- src/libcw/libcw_test.h
+++ src/libcw/libcw_test.h
@@ -1,8 +1,19 @@
+/*
+  This file is a part of unixcw project.
+  unixcw project is covered by GNU General Public License.
+*/
+
 #ifndef H_LIBCW_TEST
 #define H_LIBCW_TEST
 
 
+
+
+
 #include <stddef.h> /* size_t */
+
+
+
 
 
 static const int cw_test_print_width = 75;
@@ -20,6 +31,9 @@ static const int cw_test_print_width = 75;
 
 int cw_test_args(int argc, char *const argv[], char *sound_systems, size_t systems_max);
 void cw_test_print_help(const char *progname);
+
+
+
 
 
 #endif /* #ifndef H_LIBCW_TEST */
