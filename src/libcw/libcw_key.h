57abb460983888eccd8e32619a5b556272c8bf5b
acerion <acerion@wp.pl>
2014-08-02 21:17:40 +0200

    Adding iambic keyer fields to cw_key_t (as a nested "ik" structure). A stand-alone iambic keyer data type won't be needed anymore.

--- src/libcw/libcw_key.h
+++ src/libcw/libcw_key.h
@@ -18,7 +18,7 @@
 
 
 
-
+#if 0
 /* Iambic keyer status.  The keyer functions maintain the current
    known state of the paddles, and latch false-to-true transitions
    while busy, to form the iambic effect.  For Curtis mode B, the
@@ -52,7 +52,7 @@ typedef struct cw_iambic_keyer_struct {
 	cw_gen_t *gen;
 
 } cw_iambic_keyer_t;
-
+#endif
 
 
 
@@ -101,6 +101,42 @@ typedef struct cw_key_struct {
 		int key_value;    /* Open/Closed, Space/Mark, NoSound/Sound. */
 	} sk;
 
+
+	/* Iambic keyer.  The keyer functions maintain the current
+	   known state of the paddles, and latch false-to-true
+	   transitions while busy, to form the iambic effect.  For
+	   Curtis mode B, the keyer also latches any point where both
+	   paddle states are true at the same time. */
+	struct {
+		int graph_state;      /* State of iambic keyer state machine. */
+		int key_value;        /* Open/Closed, Space/Mark, NoSound/Sound. */
+
+		bool dot_paddle;      /* Dot paddle state */
+		bool dash_paddle;     /* Dash paddle state */
+
+		bool dot_latch;       /* Dot false->true latch */
+		bool dash_latch;      /* Dash false->true latch */
+
+		/* Iambic keyer "Curtis" mode A/B selector.  Mode A and mode B timings
+		   differ slightly, and some people have a preference for one or the other.
+		   Mode A is a bit less timing-critical, so we'll make that the default. */
+		bool curtis_mode_b;
+
+		bool curtis_b_latch;  /* Curtis Dot&&Dash latch */
+
+		bool lock;            /* FIXME: describe why we need this flag. */
+
+		struct timeval *timer; /* Timer for receiving of iambic keying, owned by client code. */
+
+		/* Generator associated with the keyer. Should never
+		   be NULL as iambic keyer *needs* a generator to
+		   function properly (and to generate audible tones).
+		   Set using
+		   cw_key_register_generator_internal(). */
+		/* No separate generator, use cw_key_t->gen. */
+	} ik;
+
+
 	/* Tone-queue key. */
 	struct {
 		int key_value;    /* Open/Closed, Space/Mark, NoSound/Sound. */
@@ -124,8 +160,8 @@ enum {
 
 
 
-int  cw_iambic_keyer_update_graph_state_internal(cw_iambic_keyer_t *keyer);
-void cw_iambic_keyer_increment_timer_internal(cw_iambic_keyer_t *keyer, int usecs);
+int  cw_iambic_keyer_update_graph_state_internal(volatile cw_key_t *keyer);
+void cw_iambic_keyer_increment_timer_internal(volatile cw_key_t *keyer, int usecs);
 //void cw_iambic_keyer_register_generator_internal(cw_iambic_keyer_t *keyer, cw_gen_t *gen);
 
 //void cw_straight_key_register_generator_internal(volatile cw_straight_key_t *key, cw_gen_t *gen);
