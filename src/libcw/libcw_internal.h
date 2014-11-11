691454ee14f5da945e8d042e4834cfe8f23db3e2
acerion <acerion@wp.pl>
2014-11-11 18:31:00 +0100

    Moving two function declarations to libcw_utils.h.

--- src/libcw/libcw_internal.h
+++ src/libcw/libcw_internal.h
@@ -20,10 +20,6 @@
 
 /* From libcw.c, needed in libcw_key.c. */
 void cw_sync_parameters_internal(cw_gen_t *gen, cw_rec_t *rec);
-void cw_finalization_schedule_internal(void);
-
-/* From libcw.c, needed in libcw_signal.c. */
-void cw_finalization_cancel_internal(void);
 
 
 
