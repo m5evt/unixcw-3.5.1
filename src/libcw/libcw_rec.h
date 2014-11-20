ffa5f4a3db8bbf6b2f5145702b29a0c0cdbe7503
acerion <acerion@wp.pl>
2014-11-20 21:28:43 +0100

    Moving most of declarations from header file to c file. The declarations don't need to be exposed to other modules.

--- src/libcw/libcw_rec.h
+++ src/libcw/libcw_rec.h
@@ -7,181 +7,22 @@
 #define H_LIBCW_REC
 
 
-#include "libcw.h"
 
 
-
-/* Morse code controls and timing parameters */
 
 /* Dot length magic number; from PARIS calibration, 1 Dot=1200000/WPM
    usec.
+
    This variable is used in generator code as well. */
 enum { CW_DOT_CALIBRATION = 1200000 };
 
-/* Default initial values for library controls. */
-enum { CW_REC_ADAPTIVE_MODE_INITIAL = false };                                            /* Initial adaptive receive setting. */
-
-/* TODO: it would be interesting to track (in debug mode) relationship
-   between "speed threshold" and "noise threshold" parameters. */
-enum { CW_REC_SPEED_THRESHOLD_INITIAL = (CW_DOT_CALIBRATION / CW_SPEED_INITIAL) * 2 };    /* Initial adaptive speed threshold. [us] */
-enum { CW_REC_NOISE_THRESHOLD_INITIAL = (CW_DOT_CALIBRATION / CW_SPEED_MAX) / 2 };        /* Initial noise filter threshold. */
 
 
 
 
-
-/* TODO: what is the relationship between this constant and CW_REC_REPRESENTATION_CAPACITY?
-   Both have value of 256. Coincidence? I don't think so. */
-enum { CW_REC_STATISTICS_CAPACITY = 256 };
-
-
-
-/* Receiver contains a fixed-length buffer for representation of received data.
-   Capacity of the buffer is vastly longer than any practical representation.
-   Don't know why, a legacy thing. */
-enum { CW_REC_REPRESENTATION_CAPACITY = 256 };
-
-
-
-/* Length of array used to calculate average length of a mark. Average
-   length of a mark is used in adaptive receiving mode to track speed
-   of incoming Morse data. */
-enum { CW_REC_AVERAGING_ARRAY_LENGTH = 4 };
-
-
-
-/* Types of receiver's timing statistics.
-   CW_REC_STAT_NONE must be zero so that the statistics buffer is initially empty. */
-typedef enum {
-	CW_REC_STAT_NONE = 0,
-	CW_REC_STAT_DOT,           /* Dot mark. */
-	CW_REC_STAT_DASH,          /* Dash mark. */
-	CW_REC_STAT_IMARK_SPACE,   /* Inter-mark space. */
-	CW_REC_STAT_ICHAR_SPACE    /* Inter-character space. */
-} stat_type_t;
-
-
-
-typedef struct {
-	stat_type_t type;  /* Record type */
-	int delta;         /* Difference between actual and ideal length of mark or space. [us] */
-} cw_rec_statistics_t;
-
-
-
-/* A moving averages structure - circular buffer. Used for calculating
-   averaged length ([us]) of dots and dashes. */
-typedef struct {
-	int buffer[CW_REC_AVERAGING_ARRAY_LENGTH];  /* Buffered mark lengths. */
-	int cursor;                                 /* Circular buffer cursor. */
-	int sum;                                    /* Running sum of lengths of marks. [us] */
-	int average;                                /* Averaged length of a mark. [us] */
-} cw_rec_averaging_t;
-
-
-
-
-typedef struct {
-	/* State of receiver state machine. */
-	int state;
-
-	int gap; /* Inter-character-gap, similar as in generator. */
-
-
-
-	/* Essential receive parameters. */
-	/* Changing values of speed, tolerance or
-	   is_adaptive_receive_mode will trigger a recalculation of
-	   low level timing parameters. */
-
-	int speed; /* [wpm] */
-	int tolerance;
-	bool is_adaptive_receive_mode;
-	int noise_spike_threshold;
-
-
-
-	/* Library variable which is automatically maintained from the
-	   Morse input stream, rather than being settable by the
-	   user.
-	   Not exactly a "speed", but for a lack of a better
-	   name... */
-	int adaptive_speed_threshold; /* [us] */
-
-
-	/* Retained timestamps of mark's begin and end. */
-	struct timeval mark_start;
-	struct timeval mark_end;
-
-	/* Buffer for received representation (dots/dashes). This is a
-	   fixed-length buffer, filled in as tone on/off timings are
-	   taken. The buffer is vastly longer than any practical
-	   representation.
-
-	   Along with it we maintain a cursor indicating the current
-	   write position. */
-	char representation[CW_REC_REPRESENTATION_CAPACITY];
-	int representation_ind;
-
-
-
-	/* Receiver's low-level timing parameters */
-
-	/* These are basic timing parameters which should be
-	   recalculated each time client code demands changing some
-	   higher-level parameter of receiver.  How these values are
-	   calculated depends on receiving mode (fixed/adaptive). */
-	int dot_len_ideal;        /* Length of an ideal dot. [microseconds]/[us] */
-	int dot_len_min;          /* Minimal length of mark that will be identified as dot. [us] */
-	int dot_len_max;          /* Maximal length of mark that will be identified as dot. [us] */
-
-	int dash_len_ideal;       /* Length of an ideal dash. [us] */
-	int dash_len_min;         /* Minimal length of mark that will be identified as dash. [us] */
-	int dash_len_max;         /* Maximal length of mark that will be identified as dash. [us] */
-
-	int eom_len_ideal;        /* Ideal end of mark, for stats */
-	int eom_len_min;          /* Shortest end of mark allowable */
-	int eom_len_max;          /* Longest end of mark allowable */
-
-	int eoc_len_ideal;        /* Ideal end of char, for stats */
-	int eoc_len_min;          /* Shortest end of char allowable */
-	int eoc_len_max;          /* Longest end of char allowable */
-
-	/* These two fields have the same function as in
-	   cw_gen_t. They are needed in function re-synchronizing
-	   parameters. */
-	int additional_delay;     /* More delay at the end of a char */
-	int adjustment_delay;     /* More delay at the end of a word */
-
-
-
-	/* Are receiver's parameters in sync?
-
-	   After changing receiver's receive speed, tolerance or
-	   adaptive mode, some receiver's internal parameters need to
-	   be re-calculated. This is a flag that shows when this needs
-	   to be done. */
-	bool parameters_in_sync;
-
-
-
-	/* Receiver statistics.
-	   A circular buffer of entries indicating the difference
-	   between the actual and the ideal length of received mark or
-	   space, tagged with the type of statistic held, and a
-	   circular buffer pointer. */
-	cw_rec_statistics_t statistics[CW_REC_STATISTICS_CAPACITY];
-	int statistics_ind;
-
-
-
-	/* Data structures for calculating averaged length of dots and
-	   dashes. The averaged lengths are used for adaptive tracking
-	   of receiver's speed (tracking of speed of incoming data). */
-	cw_rec_averaging_t dot_averaging;
-	cw_rec_averaging_t dash_averaging;
-
-} cw_rec_t;
+/* Forward declaration of a type. */
+struct cw_rec_struct;
+typedef struct cw_rec_struct cw_rec_t;
 
 
 
@@ -189,6 +30,8 @@ typedef struct {
 
 void cw_rec_reset_receive_parameters_internal(cw_rec_t *rec);
 void cw_rec_sync_parameters_internal(cw_rec_t *rec);
+
+
 
 
 
