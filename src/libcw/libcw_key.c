b572b1e78d22eeb3c33d3cc85589322fd5c68801
acerion <acerion@wp.pl>
2014-11-21 18:54:00 +0100

    Moving definition of cw_key_t from header file to c file.

--- src/libcw/libcw_key.c
+++ src/libcw/libcw_key.c
@@ -29,6 +29,8 @@
 #include <inttypes.h> /* uint32_t */
 #include <errno.h>
 #include <unistd.h>   /* usleep() */
+#include <sys/time.h>
+
 
 #include "libcw_debug.h"
 #include "libcw_key.h"
@@ -46,6 +48,86 @@ extern cw_debug_t cw_debug_object_ev;
 extern cw_debug_t cw_debug_object_dev;
 
 extern cw_rec_t cw_receiver;
+
+
+
+
+
+
+struct cw_key_struct {
+	/* Straight key and iambic keyer needs a generator to produce
+	   a sound on "Key Down" events. Maybe we don't always need a
+	   sound, but sometimes we do want to have it.
+
+	   Additionally iambic keyer needs a generator for timing
+	   purposes.
+
+	   In any case - a key needs to have access to a generator
+	   (but a generator doesn't need a key). This is why the key
+	   data type has a "generator" field, not the other way
+	   around. */
+	cw_gen_t *gen;
+
+
+	/* External "on key state change" callback function and its
+	   argument.
+
+	   It may be useful for a client to have this library control
+	   an external keying device, for example, an oscillator, or a
+	   transmitter.  Here is where we keep the address of a
+	   function that is passed to us for this purpose, and a void*
+	   argument for it. */
+	void (*key_callback)(void*, int);
+	void *key_callback_arg;
+
+
+	/* Straight key. */
+	struct {
+		int key_value;    /* Open/Closed, Space/Mark, NoSound/Sound. */
+	} sk;
+
+
+	/* Iambic keyer.  The keyer functions maintain the current
+	   known state of the paddles, and latch false-to-true
+	   transitions while busy, to form the iambic effect.  For
+	   Curtis mode B, the keyer also latches any point where both
+	   paddle states are true at the same time. */
+	struct {
+		int graph_state;       /* State of iambic keyer state machine. */
+		int key_value;         /* Open/Closed, Space/Mark, NoSound/Sound. */
+
+		bool dot_paddle;       /* Dot paddle state */
+		bool dash_paddle;      /* Dash paddle state */
+
+		bool dot_latch;        /* Dot false->true latch */
+		bool dash_latch;       /* Dash false->true latch */
+
+		/* Iambic keyer "Curtis" mode A/B selector.  Mode A and mode B timings
+		   differ slightly, and some people have a preference for one or the other.
+		   Mode A is a bit less timing-critical, so we'll make that the default. */
+		bool curtis_mode_b;
+
+		bool curtis_b_latch;   /* Curtis Dot&Dash latch */
+
+		bool lock;             /* FIXME: describe why we need this flag. */
+
+		struct timeval *timer; /* Timer for receiving of iambic keying, owned by client code. */
+
+		/* Generator associated with the keyer. Should never
+		   be NULL as iambic keyer *needs* a generator to
+		   function properly (and to generate audible tones).
+		   Set using
+		   cw_key_register_generator_internal(). */
+		/* No separate generator, use cw_key_t->gen. */
+	} ik;
+
+
+	/* Tone-queue key. */
+	struct {
+		int key_value;    /* Open/Closed, Space/Mark, NoSound/Sound. */
+	} tk;
+};
+
 
 
 
