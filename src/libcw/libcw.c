26489e26103d86c68b67fbd9f330057ddcf1de4c
acerion <acerion@wp.pl>
2015-02-18 22:26:01 +0100

    New file for code using global generator and receiver variables.
    The code will be implemented using new-style functions (accepting
    generator or receiver as first argument). This is to keep the global
    variables and functions operating on them separately from new-style
    functions.

---
+++ src/libcw/libcw.c
@@ -0,0 +1,28 @@
+/*
+  Copyright (C) 2001-2006  Simon Baldwin (simon_baldwin@yahoo.com)
+  Copyright (C) 2011-2015  Kamil Ignacak (acerion@wp.pl)
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+/**
+   \file libcw.c
+
+*/
+
+
+
+#include "libcw.h"
