97bcd8f2bbdbebd778e92c91e2e2746aa0a47cb9
acerion <acerion@wp.pl>
2015-02-22 15:45:45 +0100

    Removing references to global cw_generator variable

--- src/libcw/libcw_test_simple_gen.c
+++ src/libcw/libcw_test_simple_gen.c
@@ -51,8 +51,6 @@ extern cw_debug_t cw_debug_object;
 extern cw_debug_t cw_debug_object_ev;
 extern cw_debug_t cw_debug_object_dev;
 
-extern cw_gen_t *cw_generator;
-
 
 typedef bool (*predicate_t)(const char *device);
 static void main_helper(int audio_system, const char *name, const char *device, predicate_t predicate);
@@ -128,14 +126,6 @@ void main_helper(int audio_system, const char *name, const char *device, predica
 			cw_generator_start();
 
 			//cw_send_string("abcdefghijklmnopqrstuvwyz0123456789");
-			cw_send_string("eish ");
-			cw_wait_for_tone_queue();
-
-			cw_generator_set_tone_slope((cw_generator), CW_TONE_SLOPE_SHAPE_LINEAR, -1);
-			cw_send_string("eish ");
-			cw_wait_for_tone_queue();
-
-			cw_generator_set_tone_slope((cw_generator), CW_TONE_SLOPE_SHAPE_SINE, -1);
 			cw_send_string("eish ");
 			cw_wait_for_tone_queue();
 
