05a531d572464fc5663d286d946d44cb700596af
acerion <acerion@wp.pl>
2015-09-01 21:21:59 +0200

    Simplifying class design
    Instead of having DisplayImpl class inside of Display class, I now
    have just an extended Display class, renamed to more suitable
    TextArea.
    
    Part of this change are also some changes to display.cc.
    
    This change is reflected in modules: receiver, sender, application.

--- src/xcwcp/display.h
+++ src/xcwcp/display.h
@@ -1,65 +1,70 @@
-// Copyright (C) 2001-2006  Simon Baldwin (simon_baldwin@yahoo.com)
-// Copyright (C) 2011-2015  Kamil Ignacak (acerion@wp.pl)
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
+/*
+  This file is a part of unixcw project.
+  unixcw project is covered by GNU General Public License.
+*/
+
+#ifndef H_XCWCP_TEXTAREA
+#define H_XCWCP_TEXTAREA
 
 
-#ifndef _XCWCP_DISPLAY_H
-#define _XCWCP_DISPLAY_H
-
-#include <QString>
 
 
-//-----------------------------------------------------------------------
-//  Class Display
-//-----------------------------------------------------------------------
 
-// Describes an extremely simply text display interface.  The interface is
-// minimized and abstracted like this to make changes to the underlying
-// implementation easy.
+#include "config.h"
+
+
+
+
+
+#include <QWidget>
+#include <QTextEdit>
+#include <QEvent>
+#include <QMenu>
+
+
+
+
 
 namespace cw {
 
-class Application;
-class DisplayImpl;
+	class Application;
 
-class Display {
- public:
-	Display (Application *application, QWidget *parent);
-	QWidget *get_widget() const;
+	class TextArea : public QTextEdit {
+		//Q_OBJECT
+	public:
+		TextArea(Application *application, QWidget *parent = 0);
+		~TextArea() {};
 
-	// Minimal text display interface; add a character, remove a character,
-	// and clear the display completely.
-	void append(char c);
-	void backspace();
-	void clear();
+		QWidget *get_widget();
 
-	// Minimal pass-through status bar interface.
-	void show_status(const QString &status);
-	void clear_status();
+		void append(char c);
+		void backspace();
 
- private:
-	Application *application_;
-	DisplayImpl *implementation_;
 
-	// Prevent unwanted operations.
-	Display (const Display &);
-	Display &operator= (const Display &);
-};
+		void show_status(const QString &status);
+		void clear_status();
 
-}  // cw namespace
+	protected:
+		// Functions overridden to catch events from the parent class.
+		void keyPressEvent(QKeyEvent *event);
+		void keyReleaseEvent(QKeyEvent *event);
+		void mousePressEvent(QMouseEvent *event);
+		void mouseDoubleClickEvent(QMouseEvent *event);
+		void mouseReleaseEvent(QMouseEvent *event);
+		// Are these necessary after adding fontPointSize() in constructor?
+		virtual QMenu *createPopupMenu(const QPoint &);
+		virtual QMenu *createPopupMenu();
 
-#endif  // _XCWCP_DISPLAY_H
+	private:
+		// Application to forward key and mouse events to.
+		Application *application_;
+
+		// Prevent unwanted operations.
+		TextArea(const TextArea &);
+		TextArea &operator=(const TextArea &);
+	};
+} /* namespace cw */
+
+
+
+#endif /* #ifndef H_XCWCP_TEXTAREA */
