6273e1d889f48b63818b85abf8583e69c119f136
acerion <acerion@wp.pl>
2015-09-04 22:52:12 +0200

    Reformatting code and comments

--- src/xcwcp/receiver.h
+++ src/xcwcp/receiver.h
@@ -16,31 +16,38 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
-#ifndef _XCWCP_RECEIVER_H
-#define _XCWCP_RECEIVER_H
+#ifndef H_XCWCP_RECEIVER
+#define H_XCWCP_RECEIVER
 
 #include <QMouseEvent>
 #include <QKeyEvent>
 
 
-//-----------------------------------------------------------------------
-//  Class Receiver
-//-----------------------------------------------------------------------
 
-// Encapsulates the main application receiver data and functions.  Receiver
-// abstracts states associated with receiving, event handling, libcw keyer
-// event handling, and data passed between signal handler and foreground
-// contexts.
+
 
 namespace cw {
 
-class Application;
-class TextArea;
-class Mode;
 
-class Receiver {
- public:
-        Receiver(Application *a, TextArea *t) :
+
+
+
+	class Application;
+	class TextArea;
+	class Mode;
+
+
+
+
+
+	/* Class Receiver encapsulates the main application receiver
+	   data and functions.  Receiver abstracts states associated
+	   with receiving, event handling, libcw keyer event handling,
+	   and data passed between signal handler and foreground
+	   contexts. */
+	class Receiver {
+	public:
+		Receiver(Application *a, TextArea *t) :
 	        app (a),
 		textarea (t),
 		is_pending_inter_word_space_ (false),
@@ -49,75 +56,88 @@ class Receiver {
 		is_left_down (false),
 		is_right_down (false) { }
 
-	// Poll timeout handler.
-	void poll(const Mode *current_mode);
+		/* Poll timeout handler. */
+		void poll(const Mode *current_mode);
 
-	// Keypress event handler.
-	void handle_key_event(QKeyEvent *event, bool is_reverse_paddles);
+		/* Keyboard key event handler. */
+		void handle_key_event(QKeyEvent *event, bool is_reverse_paddles);
 
-	// Mouse button press event handler.
-	void handle_mouse_event(QMouseEvent *event, bool is_reverse_paddles);
+		/* Mouse button press event handler. */
+		void handle_mouse_event(QMouseEvent *event, bool is_reverse_paddles);
 
-	/* Straight key and iambic keyer event handler helpers. */
-	void sk_event(bool is_down);
-	void ik_left_event(bool is_down, bool is_reverse_paddles);
-	void ik_right_event(bool is_down, bool is_reverse_paddles);
+		/* Straight key and iambic keyer event handler
+		   helpers. */
+		void sk_event(bool is_down);
+		void ik_left_event(bool is_down, bool is_reverse_paddles);
+		void ik_right_event(bool is_down, bool is_reverse_paddles);
 
-	// CW library keying event handler.
-	void handle_libcw_keying_event(struct timeval *t, int key_state);
+		/* CW library keying event handler. */
+		void handle_libcw_keying_event(struct timeval *t, int key_state);
 
-	// Clear out queued data on stop, mode change, etc.
-	void clear();
+		/* Clear out queued data on stop, mode change, etc. */
+		void clear();
 
-	// Timer for measuring length of dots and dashes.
-	//
-	// Initial value of the timestamp is created by xcwcp's
-	// receiver on first "paddle down" event in a character. The
-	// timestamp is then updated by libcw on specific time
-	// intervals. The intervals are a function of keyboard key
-	// presses or mouse button presses recorded by xcwcp.
-	struct timeval timer;
+		/* Timer for measuring length of dots and dashes.
 
- private:
-	Application *app;
+		   Initial value of the timestamp is created by
+		   xcwcp's receiver on first "paddle down" event in a
+		   character. The timestamp is then updated by libcw
+		   on specific time intervals. The intervals are a
+		   function of keyboard key presses or mouse button
+		   presses recorded by xcwcp. */
+		struct timeval timer;
 
-	// Display used for output.
-	TextArea *textarea;
+	private:
+		Application *app;
+		TextArea *textarea;
 
-	// Flag indicating if receive polling has received a character, and may need
-	// to augment it with a word space on a later poll.
-	volatile bool is_pending_inter_word_space_;
+		/* Flag indicating if receive polling has received a
+		   character, and may need to augment it with a word
+		   space on a later poll. */
+		volatile bool is_pending_inter_word_space_;
 
-	// Flag indicating possible receive errno detected in signal
-	// handler context and needing to be passed to the foreground.
-	volatile int libcw_receive_errno_;
+		/* Flag indicating possible receive errno detected in
+		   signal handler context and needing to be passed to
+		   the foreground. */
+		volatile int libcw_receive_errno_;
 
-	// Safety flag to ensure that we keep the library in sync with keyer events.
-	// Without, there's a chance that of a on-off event, one half will go to one
-	// application instance, and the other to another instance.
-	volatile int tracked_key_state_;
+		/* Safety flag to ensure that we keep the library in
+		   sync with keyer events.  Without, there's a chance
+		   that of a on-off event, one half will go to one
+		   application instance, and the other to another
+		   instance. */
+		volatile int tracked_key_state_;
 
-	// State of left and right paddle of iambic keyer. The flags
-	// are common for keying with keyboard keys and with mouse
-	// buttons.
-	//
-	// A timestamp for libcw needs to be generated only in
-	// situations when one of the paddles comes down and the other
-	// is up. This is why we observe state of both paddles
-	// separately.
-	bool is_left_down;
-	bool is_right_down;
+		/* State of left and right paddle of iambic keyer. The
+		   flags are common for keying with keyboard keys and
+		   with mouse buttons.
 
-	// Poll primitives to handle receive errors, characters, and spaces.
-	void poll_report_receive_error();
-	void poll_receive_character();
-	void poll_receive_space();
+		   A timestamp for libcw needs to be generated only in
+		   situations when one of the paddles comes down and
+		   the other is up. This is why we observe state of
+		   both paddles separately. */
+		bool is_left_down;
+		bool is_right_down;
 
-	// Prevent unwanted operations.
-	Receiver (const Receiver &);
-	Receiver &operator= (const Receiver &);
-};
+		/* Poll primitives to handle receive errors,
+		   characters, and spaces. */
+		void poll_report_receive_error();
+		void poll_receive_character();
+		void poll_receive_space();
 
-}  // cw namespace
+		/* Prevent unwanted operations. */
+		Receiver(const Receiver &);
+		Receiver &operator=(const Receiver &);
+	};
 
-#endif  // _XCWCP_RECEIVER_H
+
+
+
+
+}  /* namespace cw */
+
+
+
+
+
+#endif  /* #endif H_XCWCP_RECEIVER */
