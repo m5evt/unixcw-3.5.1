fcacb54c29ab222872216891c1e8944abff2e0a6
acerion <acerion@wp.pl>
2015-09-10 19:53:47 +0200

    Reformatting code

--- src/cw/cw.h
+++ src/cw/cw.h
@@ -17,52 +17,85 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef _CW_H
-#define _CW_H
+#ifndef H_CW
+#define H_CW
+
+
+
+
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
+
+
+
+
 /* General command and query introducers. */
 enum
-{ CW_CMD_ESCAPE  = '%',  /* Command escape character */
-  CW_CMD_QUERY   = '?',  /* Query subcommand */
-  CW_CMD_CWQUERY = '>',  /* CW report query subcommand */
-  CW_CMD_END     = ';'   /* Completes an embedded command */
+{
+	CW_CMD_ESCAPE  = '%',  /* Command escape character */
+	CW_CMD_QUERY   = '?',  /* Query subcommand */
+	CW_CMD_CWQUERY = '>',  /* CW report query subcommand */
+	CW_CMD_END     = ';'   /* Completes an embedded command */
 };
+
+
+
+
 
 /* Specific command value specifiers. */
 enum
-{ CW_CMDV_FREQUENCY    = 'T',  /* CW tone */
-  CW_CMDV_VOLUME       = 'V',  /* CW volume */
-  CW_CMDV_SPEED        = 'W',  /* CW words-per-minute */
-  CW_CMDV_GAP          = 'G',  /* CW extra gaps in sending */
-  CW_CMDV_WEIGHTING    = 'K',  /* CW weighting */
-  CW_CMDV_ECHO         = 'E',  /* CW echo chars to stdout */
-  CW_CMDV_ERRORS       = 'M',  /* CW error msgs to stderr */
-  CW_CMDV_SOUND        = 'S',  /* CW creates tones. otherwise silent */
-  CW_CMDV_COMMANDS     = 'C',  /* CW responds to @... embedded cmds */
-  CW_CMDV_COMBINATIONS = 'O',  /* CW allows [..] combinations */
-  CW_CMDV_COMMENTS     = 'P',  /* CW allows {..} combinations */
-  CW_CMDV_QUIT         = 'Q'   /* CW program exit command */
+{
+	CW_CMDV_FREQUENCY    = 'T',  /* CW tone */
+	CW_CMDV_VOLUME       = 'V',  /* CW volume */
+	CW_CMDV_SPEED        = 'W',  /* CW words-per-minute */
+	CW_CMDV_GAP          = 'G',  /* CW extra gaps in sending */
+	CW_CMDV_WEIGHTING    = 'K',  /* CW weighting */
+	CW_CMDV_ECHO         = 'E',  /* CW echo chars to stdout */
+	CW_CMDV_ERRORS       = 'M',  /* CW error msgs to stderr */
+	CW_CMDV_SOUND        = 'S',  /* CW creates tones. otherwise silent */
+	CW_CMDV_COMMANDS     = 'C',  /* CW responds to @... embedded cmds */
+	CW_CMDV_COMBINATIONS = 'O',  /* CW allows [..] combinations */
+	CW_CMDV_COMMENTS     = 'P',  /* CW allows {..} combinations */
+	CW_CMDV_QUIT         = 'Q'   /* CW program exit command */
 };
+
+
+
+
 
 /* Combination and comment start and end characters. */
 enum
-{ CW_COMBINATION_START = '[',  /* Begin [..] */
-  CW_COMBINATION_END   = ']',  /* End [..] */
-  CW_COMMENT_START     = '{',  /* Begin {..} */
-  CW_COMMENT_END       = '}'   /* End {..} */
+{
+	CW_COMBINATION_START = '[',  /* Begin [..] */
+	CW_COMBINATION_END   = ']',  /* End [..] */
+	CW_COMMENT_START     = '{',  /* Begin {..} */
+	CW_COMMENT_END       = '}'   /* End {..} */
 };
+
+
+
+
 
 /* Status values - first character of stderr messages. */
 enum
-{ CW_STATUS_OK  = '=',  /* =... command accepted */
-  CW_STATUS_ERR = '?'   /* ?... error in command */
+{
+	CW_STATUS_OK  = '=',  /* =... command accepted */
+	CW_STATUS_ERR = '?'   /* ?... error in command */
 };
+
+
+
+
 
 #if defined(__cplusplus)
 }
 #endif
-#endif  /* _CW_H */
+
+
+
+
+
+#endif  /* #ifdef H_CW */
