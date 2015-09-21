2ac6e58b88d65d7caa59468fc547f68d5e273568
acerion <acerion@wp.pl>
2015-09-21 21:29:15 +0200

    Improving the qa script

--- qa_valgrind_cwcp.sh
+++ qa_valgrind_cwcp.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-valgrind --tool=memcheck --leak-check=yes --leak-check=full -v --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes --suppressions=/usr/lib/valgrind/ncurses.supp src/cwcp/.libs/cwcp $@ 2>log_valgrind_cwcp
+
+executable_name=cwcp
+executable_fullpath=src/cwcp/.libs/cwcp
+
+timestamp=`date '+%Y.%m.%d_%H.%M.%S'`
+
+valgrind --tool=memcheck \
+	 --leak-check=yes \
+	 --leak-check=full \
+	 -v \
+	 --show-reachable=yes \
+	 --track-origins=yes \
+	 --num-callers=20 \
+	 --track-fds=yes \
+	 --suppressions=/usr/lib/valgrind/ncurses.supp \
+	 --suppressions=/usr/lib/valgrind/default.supp \
+	 $executable_fullpath $@ 2>log_valgrind_"$executable_name"_"$timestamp".log
