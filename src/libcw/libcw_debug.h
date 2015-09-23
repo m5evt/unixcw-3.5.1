52dd4cdfa3bfe578d0d7531c177e69b5a12da87f
acerion <acerion@wp.pl>
2015-09-23 19:45:30 +0200

    Standardizing copyright information in header files
    libcw2.h file gets longer preamble since it's library's main header file.

--- src/libcw/libcw_debug.h
+++ src/libcw/libcw_debug.h
@@ -1,22 +1,7 @@
 /*
- * Copyright (C) 2001-2006  Simon Baldwin (simon_baldwin@yahoo.com)
- * Copyright (C) 2011-2015  Kamil Ignacak (acerion@wp.pl)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
+  This file is a part of unixcw project.
+  unixcw project is covered by GNU General Public License, version 2 or later.
+*/
 
 #ifndef H_LIBCW_DEBUG
 #define H_LIBCW_DEBUG
--- src/libcw/libcw_oss.h
+++ src/libcw/libcw_oss.h
@@ -1,6 +1,6 @@
 /*
   This file is a part of unixcw project.
-  unixcw project is covered by GNU General Public License.
+  unixcw project is covered by GNU General Public License, version 2 or later.
 */
 
 #ifndef H_LIBCW_OSS
