928ddc7112134bb742b806fc8e6ba0bea4161d6c
acerion <acerion@wp.pl>
2015-10-05 21:14:13 +0200

    Improving build script

--- qa_build_debian.sh
+++ qa_build_debian.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-
-# setup
 PACKAGE="unixcw"
-VERSION="3.4.2"
-
+VERSION="3.5.0"
 debuild_command='debuild -us -uc'
 
-REPO=`pwd`
-cd ../../build
-BUILD=`pwd`
 
-echo "REPO = " $REPO
+
+
+echo ""
+echo ""
+echo "********** Preparing directories **********"
+sleep 1;
+REPO=`pwd`
+if [ -z "$REPO"  ]; then
+    echo "REPO is empty, exiting"
+    exit
+elif [ "$REPO" == "/" ]; then
+    echo "REPO is root directory, exiting"
+    exit
+fi
+
+
+
+
+
+cd ../../build
+if [ $? == 1 ] ; then
+    echo "Failed to enter build directory, exiting"
+    exit;
+fi
+BUILD=`pwd`
+if [ ! -d $BUILD ]; then
+    echo "Build directory $BUILD doesn't exist, exiting"
+    exit;
+fi
+
+
+
+
+
+echo "REPO =  " $REPO
 echo "BUILD = " $BUILD
 
 # "-n 1" - no need to confirm a character with Enter
@@ -23,7 +51,7 @@ case $response in
         ;;
     *)
 	echo "Aborting build"
-	exit
+    exit
         ;;
 esac
 
@@ -31,59 +59,80 @@ esac
 
 
 
-if [ "$REPO" == x"" ]; then
-    echo "REPO is empty"
-    exit
-elif [ "$REPO" == x"/" ]; then
-    echo "REPO is root directory"
-    exit
-fi
-
-if [ "$BUILD" == x"" ]; then
-    echo "BUILD is empty"
-    exit
-elif [ "$BUILD" == x"/" ]; then
-    echo "BUILD is root directory"
-    exit
-fi
-
-
-
-# clean up old files, prepare brand new, empty dir
+# Clean up old files.
+echo ""
+echo ""
+echo "********** Cleaning up build directory **********"
+sleep 1;
 rm -rf $BUILD/*
 
 
 
 
+echo ""
+echo ""
+echo "********** make dist in repository **********"
+sleep 1;
 # prepare $PACKAGE_X.Y.Z.debian.tar.gz
 cd $REPO
-rm *.tar.gz
-make dist
+rm -rf *.tar.gz
+# 'make' returns zero on success
+if ! (make dist) ; then
+    echo "Failed to build 'make dist' target, exiting"
+    echo `pwd`
+    exit
+fi
+
 
 dist=`ls *tar.gz`
+if [ -z "$dist"  ]; then
+    echo "Failed to find distribution archive, exiting"
+    exit
+else 
+    echo "distribution archive = $dist"
+fi
 
+
+
+
+
+echo ""
+echo ""
+echo "********** Moving distribution to build directory **********"
+sleep 1;
 mv $dist $BUILD/
 cd $BUILD
 
 
 
+
+
 tar xvfz $dist
-dist_dir=`ls | grep -v tar.gz`
-echo "dist dir is $dist_dir"
+rm $dist
+dist_dir=`ls`
+echo "directory with contents of distribution package is $dist_dir"
 
 
-tar cvfz $PACKAGE\_$VERSION.debian.tar.gz $dist_dir/debian
-# rm -rf $dist_dir/debian
 
 
-mv $dist_dir $PACKAGE-$VERSION
-tar cvfz $PACKAGE\_$VERSION.orig.tar.gz $PACKAGE-$VERSION --exclude=debian
 
-
-# go to final build dir and start building Debian package
-cd $PACKAGE-$VERSION
 echo ""
-echo `pwd`
+echo ""
+echo "********** Preparing archives in build directory **********"
+sleep 1;
+tar cvfz $PACKAGE\_$VERSION.debian.tar.gz $dist_dir/debian
+tar cvfz $PACKAGE\_$VERSION.orig.tar.gz $dist_dir --exclude=debian
+
+
+
+
+
+echo ""
+echo ""
+echo "********** Starting build **********"
+sleep 1;
+# go to final build dir and start building Debian package
+cd $dist_dir
 eval $debuild_command
 
 
@@ -91,16 +140,19 @@ eval $debuild_command
 
 
 # "-n 1" - no need to confirm a character with Enter
-read -r -n 1 -p "Test second build in build directory? [Y/n] " response
+read -r -n 1 -p "Test second build in build directory? [y/N] " response
 echo
 case $response in
-    [Nn][Oo]|[Nn])
-	echo "Not executing second build"
-	# pass
-        ;;
-    *)
+    [yY][eE][sS]|[yY])
+        echo "OK"
 	eval $debuild_command
 	echo "Second build completed"
         ;;
+    
+    *)
+	echo "Not executing second build"
+	# pass
+        ;;
+    
 esac
 echo
