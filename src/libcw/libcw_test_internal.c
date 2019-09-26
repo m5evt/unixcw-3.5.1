62ee505eea2e37476e72a25a27ba40849ccce461
acerion <acerion@wp.pl>
2019-09-26 19:13:35 +0200

    Moving list of test functions from old test driver to new test driver (there are now some duplicates to be removed from new file)

--- src/libcw/libcw_test_internal.c
+++ src/libcw/libcw_test_internal.c
@@ -92,15 +92,6 @@ typedef unsigned int (*cw_test_function2_t)(cw_test_stats_t * stats);
 static cw_test_function_t cw_unit_tests[] = {
 
 
-	/* cw_rec module */
-#if 0
-	test_cw_rec_identify_mark_internal,
-	test_cw_rec_test_with_base_constant,
-	test_cw_rec_test_with_random_constant,
-	test_cw_rec_test_with_random_varying,
-
-	test_cw_get_receive_parameters,
-#endif
 
 	NULL
 };
@@ -108,46 +99,7 @@ static cw_test_function_t cw_unit_tests[] = {
 
 
 static cw_test_function2_t cw_unit_tests2[] = {
-	/* cw_data module */
-	test_cw_representation_to_hash_internal,
-	test_cw_representation_to_character_internal,
-	test_cw_representation_to_character_internal_speed,
-	test_character_lookups_internal,
-	test_prosign_lookups_internal,
-	test_phonetic_lookups_internal,
-	test_validate_character_and_string_internal,
-	test_validate_representation_internal,
 
-	/* cw_tq module */
-	test_cw_tq_new_delete_internal,
-	test_cw_tq_get_capacity_internal,
-	test_cw_tq_prev_index_internal,
-	test_cw_tq_next_index_internal,
-	test_cw_tq_length_internal,
-	test_cw_tq_enqueue_dequeue_internal,
-	test_cw_tq_enqueue_internal_2,
-	test_cw_tq_is_full_internal,
-	test_cw_tq_test_capacity_1,
-	test_cw_tq_test_capacity_2,
-	test_cw_tq_wait_for_level_internal,
-
-	/* cw_gen module */
-	test_cw_gen_set_tone_slope,
-	test_cw_gen_tone_slope_shape_enums,
-	test_cw_gen_new_delete,
-	test_cw_gen_forever_internal,
-
-
-	/* cw_utils module */
-	test_cw_timestamp_compare_internal,
-	test_cw_timestamp_validate_internal,
-	test_cw_usecs_to_timespec_internal,
-	test_cw_version_internal,
-	test_cw_license_internal,
-	test_cw_get_x_limits_internal,
-
-	/* cw_debug module */
-	test_cw_debug_flags_internal,
 };
 
 
--- src/libcw/tests/test_driver_all.c
+++ src/libcw/tests/test_driver_all.c
@@ -145,6 +145,16 @@ static cw_test_function_stats_t cw_unit_tests_other_s[] = {
 	test_cw_license_internal,
 	test_cw_get_x_limits_internal,
 
+#if 0
+	/* cw_utils module */
+	test_cw_timestamp_compare_internal,
+	test_cw_timestamp_validate_internal,
+	test_cw_usecs_to_timespec_internal,
+	test_cw_version_internal,
+	test_cw_license_internal,
+	test_cw_get_x_limits_internal,
+#endif
+
 	/* cw_data module */
 	test_cw_representation_to_hash_internal,
 	test_cw_representation_to_character_internal,
@@ -156,8 +166,24 @@ static cw_test_function_stats_t cw_unit_tests_other_s[] = {
 	test_validate_representation_internal,
 
 
+#if 0
+	/* cw_data module */
+	test_cw_representation_to_hash_internal,
+	test_cw_representation_to_character_internal,
+	test_cw_representation_to_character_internal_speed,
+	test_character_lookups_internal,
+	test_prosign_lookups_internal,
+	test_phonetic_lookups_internal,
+	test_validate_character_and_string_internal,
+	test_validate_representation_internal,
+#endif
+
 	/* cw_debug module */
 	test_cw_debug_flags_internal,
+#if 0
+	/* cw_debug module */
+	test_cw_debug_flags_internal,
+#endif
 
 	NULL
 };
@@ -185,6 +211,21 @@ static cw_test_function_stats_tq_t cw_unit_tests_tq[] = {
 	test_cw_tq_operations_2,
 	test_cw_tq_operations_3,
 
+#if 0
+	/* cw_tq module */
+	test_cw_tq_new_delete_internal,
+	test_cw_tq_get_capacity_internal,
+	test_cw_tq_prev_index_internal,
+	test_cw_tq_next_index_internal,
+	test_cw_tq_length_internal,
+	test_cw_tq_enqueue_dequeue_internal,
+	test_cw_tq_enqueue_internal_2,
+	test_cw_tq_is_full_internal,
+	test_cw_tq_test_capacity_1,
+	test_cw_tq_test_capacity_2,
+	test_cw_tq_wait_for_level_internal,
+#endif
+
 	NULL
 };
 
@@ -205,6 +246,14 @@ static cw_test_function_stats_gen_t cw_unit_tests_gen[] = {
 	test_cw_gen_enqueue_representations,
 	test_cw_gen_enqueue_character_and_string,
 	test_cw_gen_forever_internal,
+
+#if 0
+	test_cw_gen_set_tone_slope,
+	test_cw_gen_tone_slope_shape_enums,
+	test_cw_gen_new_delete,
+	test_cw_gen_forever_internal,
+#endif
+
 	NULL
 };
 
@@ -227,6 +276,15 @@ static cw_test_function_stats_t cw_unit_tests_rec1[] = {
 	test_cw_rec_test_with_base_constant,
 	test_cw_rec_test_with_random_constant,
 	test_cw_rec_test_with_random_varying,
+
+#if 0
+	test_cw_rec_identify_mark_internal,
+	test_cw_rec_test_with_base_constant,
+	test_cw_rec_test_with_random_constant,
+	test_cw_rec_test_with_random_varying,
+	test_cw_get_receive_parameters,
+#endif
+
 
 	NULL
 };
